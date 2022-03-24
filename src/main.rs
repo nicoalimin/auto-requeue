136bacce5408abe345db7d709a377b9616d97cf0
nicoalimin <nicoalimin@yahoo.com>
2022-03-24 15:33:02 -0700

    Change to use Azure function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-#[macro_use] extern crate rocket;
+use std::collections::HashMap;
+use std::env;
+use std::net::Ipv4Addr;
+use warp::{http::Response, Filter};
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
+#[tokio::main]
+async fn main() {
+    let example1 = warp::get()
+        .and(warp::path("api"))
+        .and(warp::path("HttpExample"))
+        .and(warp::query::<HashMap<String, String>>())
+        .map(|p: HashMap<String, String>| match p.get("name") {
+            Some(name) => Response::builder().body(format!("Hello, {}. This HTTP triggered function executed successfully.", name)),
+            None => Response::builder().body(String::from("This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.")),
+        });
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
+    let port: u16 = match env::var(port_key) {
+        Ok(val) => val.parse().expect("Custom Handler port is not a number!"),
+        Err(_) => 3000,
+    };
+
+    warp::serve(example1).run((Ipv4Addr::UNSPECIFIED, port)).await
 }
